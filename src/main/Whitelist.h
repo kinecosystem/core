d0614d7f0fce504024479bb00966bd211cea3741
ashevin <avi@kik.com>
2018-12-15 18:58:33 +0200

    Introduce Whitelist class.

--- src/main/Whitelist.h
+++ src/main/Whitelist.h
@@ -1,33 +1,15 @@
-#pragma once
-
 #include "main/Application.h"
-#include "main/ManagedDataCache.h"
-#include "ledger/LedgerManager.h"
 
 namespace stellar
 {
 
-class Whitelist : public ManagedDataCache
+class Whitelist
 {
   public:
-    Whitelist(Application& app) : ManagedDataCache(app)
-    {
-    }
-
-    size_t unwhitelistedReserve(size_t setSize);
-
-    bool isWhitelisted(std::vector<DecoratedSignature> signatures,
-                       Hash const& txHash);
-    bool isWhitelistSig(DecoratedSignature const& sig, Hash const& txHash);
-
-    virtual std::string getAccount() override;
-
-    virtual void fulfill(std::vector<DataFrame::pointer> dfs) override;
+    Whitelist(Application& app);
+    bool isWhitelisted(std::vector<DecoratedSignature> signatures, Hash const& txHash);
 
   private:
-    std::unordered_map<uint32_t, std::vector<string64>> whitelist;
-
-	// default to a 5% reserve
-	double mReserve = 0.05;
+    std::unordered_map<uint32_t, std::vector<string64>> hash;
 };
 } // namespace stellar
