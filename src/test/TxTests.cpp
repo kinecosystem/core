stellar/stellar-core

reworked logic for testing transactions (applyCheck)
allows to perform more checks directly there instead of having
the caller perform those;
also removed some exceptions that are not relevant

src/test/TxTests.cpp
@@ -41,48 +41,72 @@ namespace txtest
 {
 
 bool
-applyCheck(TransactionFramePtr tx, Application& app)
+applyCheck(TransactionFramePtr tx, Application& app, bool checkSequm)
 {
     app.getDatabase().clearPreparedStatementCache();
 
     LedgerDelta delta(app.getLedgerManager().getCurrentLedgerHeader(),
                       app.getDatabase());
 
-    auto txSet = std::make_shared<TxSetFrame>(
-        app.getLedgerManager().getLastClosedLedgerHeader().hash);
-    txSet->add(tx);
-
-    // TODO: maybe we should just close ledger with tx instead of checking all
-    // of
-    // that manually?
+    AccountEntry srcAccountBefore;
 
     bool check = tx->checkValid(app, 0);
     TransactionResult checkResult = tx->getResult();
 
     REQUIRE((!check || checkResult.result.code() == txSUCCESS));
 
-    bool doApply;
-    // valid transaction sets ensure that
+    // now, check what happens when simulating what happens during a ledger
+    // close and reconcile it with the return value of "apply" with the one from
+    // checkValid:
+    // * an invalid (as per isValid) tx is still invalid during apply (and the
+    // same way)
+    // * a valid tx can fail later
+    auto code = checkResult.result.code();
+    if (code != txNO_ACCOUNT)
     {
-        auto code = checkResult.result.code();
-        if (code != txNO_ACCOUNT && code != txBAD_SEQ && code != txBAD_AUTH)
+        auto acnt = loadAccount(tx->getSourceID(), app, true);
+        srcAccountBefore = acnt->getAccount();
+
+        // no account -> can't process the fee
+        tx->processFeeSeqNum(delta, app.getLedgerManager());
+
+        // verify that the fee got processed
+        auto added = delta.added();
+        REQUIRE(added.begin() == added.end());
+        auto deleted = delta.deleted();
+        REQUIRE(deleted.begin() == deleted.end());
+        auto modified = delta.modified();
+        REQUIRE(modified.begin() != modified.end());
+        int modifiedCount = 0;
+        for (auto m : modified)
         {
-            tx->processFeeSeqNum(delta, app.getLedgerManager());
-            doApply = true;
-        }
-        else
-        {
-            doApply = (code != txBAD_SEQ);
+            modifiedCount++;
+            REQUIRE(modifiedCount == 1);
+            REQUIRE(m.key.account().accountID == tx->getSourceID());
+            auto& prevAccount = m.previous->mEntry.data.account();
+            REQUIRE(prevAccount == srcAccountBefore);
+            auto curAccount = m.current->mEntry.data.account();
+            // the balance should have changed
+            REQUIRE(curAccount.balance < prevAccount.balance);
+            curAccount.balance = prevAccount.balance;
+            if (app.getLedgerManager().getCurrentLedgerVersion() <= 9)
+            {
+                // v9 and below, we also need to verify that the sequence number
+                // also got processed at this time
+                REQUIRE(curAccount.seqNum == (prevAccount.seqNum + 1));
+                curAccount.seqNum = prevAccount.seqNum;
+            }
+            REQUIRE(curAccount == prevAccount);
         }
     }
 
     bool res = false;
 
-    if (doApply)
     {
+        LedgerDelta applyDelta(delta);
         try
         {
-            res = tx->apply(delta, app);
+            res = tx->apply(applyDelta, app);
         }
         catch (...)
         {
@@ -91,14 +115,60 @@ applyCheck(TransactionFramePtr tx, Application& app)
 
         REQUIRE((!res || tx->getResultCode() == txSUCCESS));
 
+        // checks that the failure is the same if pre checks failed
         if (!check)
         {
             REQUIRE(checkResult == tx->getResult());
         }
-    }
-    else
-    {
-        res = check;
+
+        if (code != txNO_ACCOUNT)
+        {
+            auto srcAccountAfter =
+                txtest::loadAccount(srcAccountBefore.accountID, app, false);
+            if (srcAccountAfter)
+            {
+                bool earlyFailure =
+                    (code == txMISSING_OPERATION || code == txTOO_EARLY ||
+                     code == txTOO_LATE || code == txINSUFFICIENT_FEE ||
+                     code == txBAD_SEQ);
+                // verify that the sequence number changed (v10+)
+                // do not perform the check if there was a failure before
+                // or during the sequence number processing
+                if (checkSequm &&
+                    app.getLedgerManager().getCurrentLedgerVersion() >= 10 &&
+                    !earlyFailure)
+                {
+                    REQUIRE(srcAccountAfter->getSeqNum() ==
+                            (srcAccountBefore.seqNum + 1));
+                }
+                // on failure, no other changes should have been made
+                if (!res)
+                {
+                    auto added = applyDelta.added();
+                    REQUIRE(added.begin() == added.end());
+                    auto deleted = applyDelta.deleted();
+                    REQUIRE(deleted.begin() == deleted.end());
+                    auto modified = applyDelta.modified();
+                    if (earlyFailure ||
+                        app.getLedgerManager().getCurrentLedgerVersion() <= 9)
+                    {
+                        // no changes during an early failure
+                        REQUIRE(modified.begin() == modified.end());
+                    }
+                    else
+                    {
+                        REQUIRE(modified.begin() != modified.end());
+                        for (auto m : modified)
+                        {
+                            REQUIRE(m.key.account().accountID ==
+                                    srcAccountBefore.accountID);
+                            // could check more here if needed
+                        }
+                    }
+                }
+            }
+        }
+        applyDelta.commit();
     }
 
     // validates db state

kinecosystem/core

src/test/TxTests.cpp
@@ -11,7 +11,6 @@
 #include "ledger/LedgerDelta.h"
 #include "lib/catch.hpp"
 #include "main/Application.h"
-#include "main/Whitelist.h"
 #include "test/TestExceptions.h"
 #include "test/TestUtils.h"
 #include "test/test.h"
@@ -42,48 +41,72 @@ namespace txtest
 {
 
 bool
-applyCheck(TransactionFramePtr tx, Application& app)
+applyCheck(TransactionFramePtr tx, Application& app, bool checkSeqNum)
 {
     app.getDatabase().clearPreparedStatementCache();
 
     LedgerDelta delta(app.getLedgerManager().getCurrentLedgerHeader(),
                       app.getDatabase());
 
-    auto txSet = std::make_shared<TxSetFrame>(
-        app.getLedgerManager().getLastClosedLedgerHeader().hash);
-    txSet->add(tx);
-
-    // TODO: maybe we should just close ledger with tx instead of checking all
-    // of
-    // that manually?
+    AccountEntry srcAccountBefore;
 
     bool check = tx->checkValid(app, 0);
     TransactionResult checkResult = tx->getResult();
 
     REQUIRE((!check || checkResult.result.code() == txSUCCESS));
 
-    bool doApply;
-    // valid transaction sets ensure that
+    // now, check what happens when simulating what happens during a ledger
+    // close and reconcile it with the return value of "apply" with the one from
+    // checkValid:
+    // * an invalid (as per isValid) tx is still invalid during apply (and the
+    // same way)
+    // * a valid tx can fail later
+    auto code = checkResult.result.code();
+    if (code != txNO_ACCOUNT)
     {
-        auto code = checkResult.result.code();
-        if (code != txNO_ACCOUNT && code != txBAD_SEQ && code != txBAD_AUTH)
+        auto acnt = loadAccount(tx->getSourceID(), app, true);
+        srcAccountBefore = acnt->getAccount();
+
+        // no account -> can't process the fee
+        tx->processFeeSeqNum(delta, app.getLedgerManager());
+
+        // verify that the fee got processed
+        auto added = delta.added();
+        REQUIRE(added.begin() == added.end());
+        auto deleted = delta.deleted();
+        REQUIRE(deleted.begin() == deleted.end());
+        auto modified = delta.modified();
+        REQUIRE(modified.begin() != modified.end());
+        int modifiedCount = 0;
+        for (auto m : modified)
         {
-            tx->processFeeSeqNum(delta, app.getLedgerManager(), app);
-            doApply = true;
-        }
-        else
-        {
-            doApply = (code != txBAD_SEQ);
+            modifiedCount++;
+            REQUIRE(modifiedCount == 1);
+            REQUIRE(m.key.account().accountID == tx->getSourceID());
+            auto& prevAccount = m.previous->mEntry.data.account();
+            REQUIRE(prevAccount == srcAccountBefore);
+            auto curAccount = m.current->mEntry.data.account();
+            // the balance should have changed
+            REQUIRE(curAccount.balance < prevAccount.balance);
+            curAccount.balance = prevAccount.balance;
+            if (app.getLedgerManager().getCurrentLedgerVersion() <= 9)
+            {
+                // v9 and below, we also need to verify that the sequence number
+                // also got processed at this time
+                REQUIRE(curAccount.seqNum == (prevAccount.seqNum + 1));
+                curAccount.seqNum = prevAccount.seqNum;
+            }
+            REQUIRE(curAccount == prevAccount);
         }
     }
 
     bool res = false;
 
-    if (doApply)
     {
+        LedgerDelta applyDelta(delta);
         try
         {
-            res = tx->apply(delta, app);
+            res = tx->apply(applyDelta, app);
         }
         catch (...)
         {
@@ -92,14 +115,60 @@ applyCheck(TransactionFramePtr tx, Application& app)
 
         REQUIRE((!res || tx->getResultCode() == txSUCCESS));
 
+        // checks that the failure is the same if pre checks failed
         if (!check)
         {
             REQUIRE(checkResult == tx->getResult());
         }
-    }
-    else
-    {
-        res = check;
+
+        if (code != txNO_ACCOUNT)
+        {
+            auto srcAccountAfter =
+                txtest::loadAccount(srcAccountBefore.accountID, app, false);
+            if (srcAccountAfter)
+            {
+                bool earlyFailure =
+                    (code == txMISSING_OPERATION || code == txTOO_EARLY ||
+                     code == txTOO_LATE || code == txINSUFFICIENT_FEE ||
+                     code == txBAD_SEQ);
+                // verify that the sequence number changed (v10+)
+                // do not perform the check if there was a failure before
+                // or during the sequence number processing
+                if (checkSeqNum &&
+                    app.getLedgerManager().getCurrentLedgerVersion() >= 10 &&
+                    !earlyFailure)
+                {
+                    REQUIRE(srcAccountAfter->getSeqNum() ==
+                            (srcAccountBefore.seqNum + 1));
+                }
+                // on failure, no other changes should have been made
+                if (!res)
+                {
+                    auto added = applyDelta.added();
+                    REQUIRE(added.begin() == added.end());
+                    auto deleted = applyDelta.deleted();
+                    REQUIRE(deleted.begin() == deleted.end());
+                    auto modified = applyDelta.modified();
+                    if (earlyFailure ||
+                        app.getLedgerManager().getCurrentLedgerVersion() <= 9)
+                    {
+                        // no changes during an early failure
+                        REQUIRE(modified.begin() == modified.end());
+                    }
+                    else
+                    {
+                        REQUIRE(modified.begin() != modified.end());
+                        for (auto m : modified)
+                        {
+                            REQUIRE(m.key.account().accountID ==
+                                    srcAccountBefore.accountID);
+                            // could check more here if needed
+                        }
+                    }
+                }
+            }
+        }
+        applyDelta.commit();
     }
 
     // validates db state
@@ -113,9 +182,7 @@ void
 checkTransaction(TransactionFrame& txFrame, Application& app)
 {
     REQUIRE(txFrame.getResult().feeCharged ==
-            (txFrame.isWhitelisted(app)
-             ? 0
-             : app.getLedgerManager().getTxFee() * txFrame.getOperations().size()));
+            app.getLedgerManager().getTxFee());
     REQUIRE((txFrame.getResultCode() == txSUCCESS ||
              txFrame.getResultCode() == txFAILED));
 }
@@ -259,19 +326,6 @@ changeTrust(Asset const& asset, int64_t limit)
     op.body.type(CHANGE_TRUST);
     op.body.changeTrustOp().limit = limit;
     op.body.changeTrustOp().line = asset;
-
-    return op;
-}
-
-Operation
-changeTrust(Asset const& asset, int64_t limit, AccountID account)
-{
-    Operation op;
-
-    op.body.type(CHANGE_TRUST);
-    op.body.changeTrustOp().limit = limit;
-    op.body.changeTrustOp().line = asset;
-    op.sourceAccount.activate() = account;
 
     return op;
 }
@@ -673,5 +727,5 @@ checkTx(int index, TxSetResultMeta& r, TransactionResultCode expected,
     checkTx(index, r, expected);
     REQUIRE(r[index].first.result.result.results()[0].code() == code);
 };
-} // namespace txtest
-} // namespace stellar
+}
+}